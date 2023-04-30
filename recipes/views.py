4343e1c5a81409f19dfdfff6de475ffc56d98110
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2023-04-30 20:46:41 -0300

    aula 63

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
+
+
 
 
 def home(request):
@@ -8,16 +10,19 @@ def home(request):
     return render(request, 'recipes/pages/home.html',
                   context={
                     'recipes': recipes,
-                  #  [make_recipe() for _ in range(10)]
+                    # [make_recipe() for _ in range(10)]
                     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html',
+      recipes = get_list_or_404(
+       Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/category.html',
                   context={
                     'recipes': recipes,
-                  #  [make_recipe() for _ in range(10)]
+                    'title': f'{recipes[0].category.name} - Category | '                 
                     })
 
 
@@ -25,5 +30,5 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html',
                   context={
                     'recipe': make_recipe(),
-                    'is_detail_page':True, 
-                     })
+                    'is_detail_page': True,
+                    })
