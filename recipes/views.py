338f9707a30d69f3e32e6eee87b1c3fc0440fdbe
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2023-05-05 20:42:47 -0300

    perdido

--- recipes/views.py
+++ recipes/views.py
@@ -15,7 +15,7 @@ def home(request):
 
 
 def category(request, category_id):
-      recipes = get_list_or_404(
+    recipes = get_list_or_404(
        Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
     )
 
