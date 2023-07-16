992568ac9426ad3855503a940f839aa471272b2f
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2023-07-16 14:08:19 -0300

    aula 87

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Category, Recipe 
 
-
-# Register your models here.
+from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
@@ -14,4 +13,3 @@ class RecipeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category, CategoryAdmin)
-#admin.site.register(Recipe, RecipeAdmin)
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,15 +1,14 @@
-from django.test import TestCase
-from django.urls import reverse, resolve
+from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Category, Recipe, User
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
-    def test_recipe_home_views_function_is_correct(self):
+class RecipeViewsTest(RecipeTestBase):
+    def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
-    def test_recipe_home_view_return_status_code_200_ok(self):
+    def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
@@ -17,53 +16,80 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
-    def test_recipe_home_show_no_recipies_if_no(self):
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
-        self.assertIn(response.content.decode('utf-8'),
-                      'NENHUMA RECEITA CADASTRADA AINDA')
-
-    def recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='password',
-            email='username@email.com',
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
         )
-        recipe = Recipe.objects.create(
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='Recipe-Slug',
-            preparation_time=10,
-            preparation_time_unit='minutos',
-            servings=5,
-            servings_unit='pessoas',
-            preparation_steps='paso de preparacao',
-            preparation_steps_is_html=False,
-            is_published=True,
-            category=category,
-            author=author,
-        )
-        ...
 
-    def test_recipe_category_views_function_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+    def test_recipe_home_template_loads_recipes(self):
+        # Need a recipe for this test
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        # Check if one recipe exists
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_home_template_do_not_load_not_publish_recipes(self):
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Check if one recipe exists
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
         self.assertIs(view.func, views.category)
 
-    def test_recipe_category_view_return_status_code_200_ok(self):
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertEqual(response.status_code, 200)
-
-    def test_recipe_category_view_return_status_code_404_ok(self):
-        response = self.client.get(
-            reverse('recipes:category', kwargs={'category_id': 1000}))
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
         self.assertEqual(response.status_code, 404)
 
-    def test_recipe_detail_views_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = "This is a category test"
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
         self.assertIs(view.func, views.recipe)
 
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
 
-# URL PA RTE NS NO SETTINGs
+    def test_recipe_detail_template_loads_recipes(self):
+        needed_title = "This is a detail test - its loads 1 recipe"
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get('recipes:recipe', kwargs={'id': 1})
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        #self.assertIn(needed_title, content)
+        print(content)
