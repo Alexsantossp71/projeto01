1b7fd122775f29bd12b837650a2362d37ec4806f
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2023-08-13 09:59:59 -0300

    aula 116

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,18 +2,36 @@ import math
 
 
 def make_pagination_range(
-        page_range,
-        qty_pages,
-        current_page,
+    page_range,
+    qty_pages,
+    current_page,
 ):
-    middle_range = math.ceil(qty_page / 2)
+    middle_range = math.ceil(qty_pages / 2)
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
-
+    total_pages = len(page_range)
     start_range_offset = abs(start_range) if start_range < 0 else 0
 
-    if start_range_offset < 0:
+    if start_range < 0:
         start_range = 0
         stop_range += start_range_offset
 
-    return page_range[start_range: stop_range]
+    if stop_range >= total_pages:
+        stop_range = total_pages
+        start_range = total_pages - qty_pages
+        ...
+
+    pagination = page_range[start_range: stop_range]
+    return {
+        'pagination': pagination,
+        'page_range': page_range,
+        'qty_pages': qty_pages,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start_range,
+        'stop_range': stop_range,
+        'first_page_is_out_of_range': current_page > middle_range,
+        'last_page_is_out_of_range': current_page < total_pages,
+
+
+    }
