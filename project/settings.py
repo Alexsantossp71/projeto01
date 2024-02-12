46262003cb5fd56fd61e67fdbb6fb265b3aeb8a7
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2024-02-12 19:44:58 -0300

    novo import no settings

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants 
-
+from typing import List
 from dotenv import load_dotenv
 load_dotenv()
 
